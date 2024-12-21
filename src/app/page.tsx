77441a3f41446c32db2c78177ad3dd3246445e14
leeuwz <jasperbos1234@gmail.com>
2024-12-21 22:42:23 +0100

    update home page gap on mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Navbar from "./components/navigation/navbar";
 import Footer from "./components/navigation/footer";
 import Introduction from "./components/introduction/Introduction";
@@ -7,11 +6,11 @@ import Grid from "./components/etc/grid";
 
 export default function Home() {
   return (
-    <main className="relative">
+    <main>
       <Grid />
       <div className="container mx-auto max-w-[920px] grid grid-rows-[30px_1fr_30px] justify-items-center min-h-screen p-8 pb-20 gap-0 sm:p-10 font-[family-name:var(--font-geist-sans)]">
         <Navbar />
-        <div className="flex-col flex py-20 gap-20 w-full">
+        <div className="flex-col flex py-20 gap-0 sm:gap-20 w-full">
           <Introduction />
           <AboutGrid />
         </div>
