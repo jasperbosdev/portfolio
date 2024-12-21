d7dfb6b1316f9e9cc358a0265f2e6db67b698cbf
leeuwz <jasperbos1234@gmail.com>
2024-12-21 22:41:27 +0100

    scale text size for typed-about on mobile

--- src/app/components/introduction/components/typed-about.tsx
+++ src/app/components/introduction/components/typed-about.tsx
@@ -4,7 +4,7 @@ import { ReactTyped } from "react-typed";
 
 export default function TypedAbout() {
     return (
-        <p className="text2xl text-neutral-600 dark:text-neutral-300 font-medium">
+        <p className="text-xl sm:text-2xl text-neutral-600 dark:text-neutral-300 font-medium">
             <ReactTyped
             strings={["Frontend developer met ervaring.", "Houd van het bouwen van simpele UI/UX oplossingen.", "Word vrolijk van enthousiaste en tevreden klanten.",
                 "In vrije tijd veel bezig met eigen projecten."
